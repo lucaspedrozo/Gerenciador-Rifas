9f1bd5d2c03337b33a281fc5cafab7a5466d98a3
lucaspedrozo <lucasdasilvapedrozo12121@gmail.com>
2022-07-26 11:03:05 -0300

    commit

---
+++ src/vendedor/controle/new-vendedor.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('.btn-new').click(function(e) {
+        e.preventDefault()
+
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+
+        $('.modal-title').append('Adicionar novo vendedor')
+
+        $('.modal-body').load('src/vendedor/visao/form-vendedor.html', function(){
+            $.ajax({
+                dataType: 'json',
+                type: 'POST' ,
+                assync: true,
+                url: 'src/tipo/modelo/al-tipo.php',
+                sucess: function(dados){
+                    for(const result of dados) {
+                        $('#TIPO_ID').append(`<option value="${result.ID}">${result.NOME}</option>`)
+
+                    }
+                }
+
+            })
+
+
+        })
+
+        $('.btn-save').show()
+
+        $('.btn-save').attr('data-operation', 'insert')
+
+        $('#modal-vendedor').modal('show')
+    })
+
+    $('.close, #close').click(function(e) {
+        e.preventDefault()
+        $('#modal-vendedor').modal('hide')
+    })
+})
